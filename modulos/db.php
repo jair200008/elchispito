d0fdefba7005ce8d033c11fde8a72e2a6a3d46ad
Jair Gaona <jairgaona60@gmail.com>
2024-05-10 09:03:45 -0500

    base de datos

--- modulos/db.php
+++ modulos/db.php
@@ -1,11 +1,7 @@
 ﻿<?php  
-$usuario = "root";
-$contraseña = "";
-<<<<<<< HEAD
+$usuario = "id22144283_jair";
+$contraseña = "Jair2008.";
 const NOMBRE_BASE_DE_DATOS = "id22144283_esquema";
-=======
-const NOMBRE_BASE_DE_DATOS = "okventa_by_parzibyte";
->>>>>>> 1f2055bc46406114baddd61be3854df0cb982548
 try {
     $base_de_datos = new PDO('mysql:host=localhost;dbname=' . NOMBRE_BASE_DE_DATOS, $usuario, $contraseña);
     $base_de_datos->query("set names utf8;");
