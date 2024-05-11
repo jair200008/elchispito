eb2b696097d0ae1dcff6877195583ee1d8bbe561
Jair Gaona <jairgaona60@gmail.com>
2024-05-11 11:46:00 -0500

    base de datos

--- modulos/db.php
+++ modulos/db.php
@@ -1,9 +1,9 @@
 ﻿﻿<?php  
-$usuario = "hzhfwpoyby";
-$contraseña = "Jair2080.";
-const NOMBRE_BASE_DE_DATOS = "elchispito-database";
+$usuario = "root";
+$contraseña = "GKUbMwoxwblSiGvUTMXNQYKPMFTPleVa";
+const NOMBRE_BASE_DE_DATOS = "railway";
 try {
-    $base_de_datos = new PDO('mysql:host=elchispito-server.mysql.database.azure.com;dbname=' . NOMBRE_BASE_DE_DATOS, $usuario, $contraseña);
+    $base_de_datos = new PDO('mysql:host=roundhouse.proxy.rlwy.net;dbname=' . NOMBRE_BASE_DE_DATOS, $usuario, $contraseña);
     $base_de_datos->query("set names utf8;");
 	$base_de_datos->setAttribute( PDO::ATTR_EMULATE_PREPARES, FALSE );
 	$base_de_datos->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
