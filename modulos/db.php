87176e9ade84df36dc684b6c286f7c8293f835a1
Jair Gaona <jairgaona60@gmail.com>
2024-05-11 17:11:13 -0500

    bases de datos

--- modulos/db.php
+++ modulos/db.php
@@ -1,9 +1,9 @@
 ﻿<?php  
-$usuario = "root";
-$contraseña = "GKUbMwoxwblSiGvUTMXNQYKPMFTPleVa";
-const NOMBRE_BASE_DE_DATOS = "esquema";
+$usuario = "if0_36529662";
+$contraseña = "itlwHTchnLmOAej";
+const NOMBRE_BASE_DE_DATOS = "if0_36529662_okventa_by_parzibyte";
 try {
-    $base_de_datos = new PDO('mysql:host=roundhouse.proxy.rlwy.net;dbname=' . NOMBRE_BASE_DE_DATOS, $usuario, $contraseña);
+    $base_de_datos = new PDO('mysql:host=sql307.infinityfree.com;dbname=' . NOMBRE_BASE_DE_DATOS, $usuario, $contraseña);
     $base_de_datos->query("set names utf8;");
 	$base_de_datos->setAttribute( PDO::ATTR_EMULATE_PREPARES, FALSE );
 	$base_de_datos->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
