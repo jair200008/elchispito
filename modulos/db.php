d50777bbae9ffa56a5b07de6f85b0c26aab82522
Jair Gaona <jairgaona60@gmail.com>
2024-05-11 17:53:30 -0500

    bases de datos

--- modulos/db.php
+++ modulos/db.php
@@ -1,9 +1,9 @@
 ﻿<?php  
-$usuario = "if0_36529662";
-$contraseña = "itlwHTchnLmOAej";
-const NOMBRE_BASE_DE_DATOS = "if0_36529662_okventa_by_parzibyte";
+$usuario = "edwar";
+$contraseña = "Jair0820.";
+const NOMBRE_BASE_DE_DATOS = "elchispito";
 try {
-    $base_de_datos = new PDO('mysql:host=sql307.infinityfree.com;dbname=' . NOMBRE_BASE_DE_DATOS, $usuario, $contraseña);
+    $base_de_datos = new PDO('mysql:host=eighth-study-423022-f8:us-central1:esquema;dbname=' . NOMBRE_BASE_DE_DATOS, $usuario, $contraseña);
     $base_de_datos->query("set names utf8;");
 	$base_de_datos->setAttribute( PDO::ATTR_EMULATE_PREPARES, FALSE );
 	$base_de_datos->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
